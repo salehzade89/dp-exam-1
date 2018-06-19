4d33695b4d234cfcdc1e009ebb4464b25dc48aec
Alim Salehzade <salehzade89@gmail.com>
2018-06-19 20:39:30 +0400

    .

--- Project13/main.cpp
+++ Project13/main.cpp
@@ -1,13 +1,27 @@
 #include <iostream>
 #include <string>
 
-class TextInput
+struct ITextInput
+{
+	virtual const std::string & getText() const noexcept = 0;
+	virtual void setText(std::string& text) noexcept = 0;
+	virtual void show() const noexcept = 0;
+};
+
+struct IButton
+{
+	virtual const std::string & getText() const noexcept = 0;
+	virtual void setText(std::string& text) noexcept = 0;
+	virtual void show() const noexcept = 0;
+};
+
+class WindowsTextInput : public ITextInput
 {
 	std::string text;
 	uint32_t id;
 public:
-	TextInput() = default;
-	TextInput(const std::string& text) :text(text) {}
+	WindowsTextInput() = default;
+	WindowsTextInput(const std::string& text) :text(text) {}
 	const std::string & getText() const noexcept
 	{
 		return text;
@@ -22,13 +36,55 @@ public:
 	}
 };
 
-class Button
+class WindowsButton :public IButton
 {
 	std::string text;
 	uint32_t id;
 public:
-	Button() = default;
-	Button(const std::string& text) :text(text) {}
+	WindowsButton() = default;
+	WindowsButton(const std::string& text) :text(text) {}
+	const std::string & getText() const noexcept
+	{
+		return text;
+	}
+	void setText(std::string& text) noexcept
+	{
+		this->text = text;
+	}
+	void show() const noexcept
+	{
+		std::cout << "Text : " << text << '\n';
+	}
+};
+
+class LinuxTextInput : public ITextInput
+{
+	std::string text;
+	uint32_t id;
+public:
+	LinuxTextInput() = default;
+	LinuxTextInput(const std::string& text) :text(text) {}
+	const std::string & getText() const noexcept
+	{
+		return text;
+	}
+	void setText(std::string& text) noexcept
+	{
+		this->text = text;
+	}
+	void show() const noexcept
+	{
+		std::cout << "Text : " << text << '\n';
+	}
+};
+
+class LinuxButton :public IButton
+{
+	std::string text;
+	uint32_t id;
+public:
+	LinuxButton() = default;
+	LinuxButton(const std::string& text) :text(text) {}
 	const std::string & getText() const noexcept
 	{
 		return text;
@@ -45,31 +101,31 @@ public:
 
 struct Factory
 {
-	virtual Button* createButton() const noexcept = 0;
-	virtual TextInput* createTextInput() const noexcept = 0;
+	virtual IButton* createButton() const noexcept = 0;
+	virtual ITextInput* createTextInput() const noexcept = 0;
 };
 
 struct WindowsFactory :public Factory
 {
-	Button* createButton() const noexcept override
+	IButton* createButton() const noexcept override
 	{
-		return new Button("struct WindowsFactory :public Factory");
+		return new WindowsButton("struct WindowsFactory :public Factory");
 	}
-	TextInput * createTextInput()const noexcept override
+	ITextInput * createTextInput()const noexcept override
 	{
-		return new TextInput("struct WindowsFactory :public Factory");
+		return new WindowsTextInput("struct WindowsFactory :public Factory");
 	}
 };
 
 struct LinuxFactory :public Factory
 {
-	Button* createButton() const noexcept override
+	IButton* createButton() const noexcept override
 	{
-		return new Button("struct LinuxFactory :public Factory");
+		return new LinuxButton("struct LinuxFactory :public Factory");
 	}
-	TextInput * createTextInput() const noexcept override
+	ITextInput * createTextInput() const noexcept override
 	{
-		return new TextInput("struct LinuxFactory :public Factory");
+		return new LinuxTextInput("struct LinuxFactory :public Factory");
 	}
 };
 
@@ -77,7 +133,7 @@ int main()
 {
 	LinuxFactory linuxFactory;
 
-	Button * button = linuxFactory.createButton();
+	IButton * button = linuxFactory.createButton();
 
 	button->show();
 
